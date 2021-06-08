936404ba42996f6ef9dcc6a41c343fd10ff12328
Alex Kacijan <alex.kacijan@C02CRDRBMD6M.local>
2021-06-08 13:37:17 +0200

    Update Docker folder

--- docker/php/setup.sh
+++ docker/php/setup.sh
@@ -13,12 +13,12 @@ chown -R www-data /var/www/html
 runuser -g www-data -u www-data -- /usr/bin/composer install --no-interaction
 runuser -g www-data -u www-data -- /usr/bin/make assets
 
-
+mkdir log app/logs
 chmod +w -R admin-dev/autoupgrade app/config app/logs app/Resources/translations cache config download img log mails modules themes translations upload var
 
-php install-dev/index_cli.php --db_server=db --db_password=root --db_name=prestashop \
-    --name=ExampleShop --country=de --language=de --firstname=Max --lastname=Musterman --password=admin123 --email=admin@admin.com \
-      && mv install-dev __install-dev \
-      && mv admin* backend
+php install-dev/index_cli.php --db_server=db --db_password=root --db_name=prestashop --name=ExampleShop --country=de --language=de --firstname=Max --lastname=Musterman --password=admin123 --email=admin@admin.com
+
+mv install-dev __install-dev
+mv admin* backend
 
 tail -f /dev/stdout
