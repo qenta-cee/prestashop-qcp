0f5c32296b00a307e99dc5f9645d3af906b56261
Alex Kacijan <alex.kacijan@C02CRDRBMD6M.local>
2021-06-08 11:39:39 +0200

    updating setup.sh files

--- docker/php/setup.sh
+++ docker/php/setup.sh
@@ -6,14 +6,12 @@ chmod +x /tmp/wait_for_service.sh
 /tmp/wait_for_service.sh ${MYSQL_HOST} 3306
 
 cd /var/www/html 
-ls
-git clone --depth 1 --branch ${PRESTASHOP_VERSION} https://github.com/PrestaShop/PrestaShop.git
 
-cd PrestaShop
+git clone --depth 1 --branch ${PRESTASHOP_VERSION} https://github.com/PrestaShop/PrestaShop.git /var/www/html
 
-
-su www-data -c 'composer install' 
-make assets
+chown -R www-data /var/www/html
+runuser -g www-data -u www-data -- /usr/bin/composer install --no-interaction
+runuser -g www-data -u www-data -- /usr/bin/make assets
 
 
 chmod +w -R admin-dev/autoupgrade app/config app/logs app/Resources/translations cache config download img log mails modules themes translations upload var
@@ -22,7 +20,5 @@ php install-dev/index_cli.php --db_server=db --db_password=root --db_name=presta
     --name=ExampleShop --country=de --language=de --firstname=Max --lastname=Musterman --password=admin123 --email=admin@admin.com \
       && mv install-dev __install-dev \
       && mv admin* backend
-
-chmod 777 /var/www/html/PrestaShop/*
 
 tail -f /dev/stdout
