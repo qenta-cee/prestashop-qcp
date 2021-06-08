d29a7d6d7c0274630eb7dab8bd2263078eb744f3
Herbert Knapp <42992894+HMKnapp@users.noreply.github.com>
2021-06-08 14:33:19 +0200

    Update scripts

--- docker/php/setup.sh
+++ docker/php/setup.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
-apache2-foreground > /dev/null 2>&1 &
+set -xe
+
+apache2-foreground # > /dev/null 2>&1 &
 
 chmod +x /tmp/wait_for_service.sh
 /tmp/wait_for_service.sh ${MYSQL_HOST} 3306
 
 cd /var/www/html 
+git clone --depth 1 --branch ${PRESTASHOP_VERSION} https://github.com/PrestaShop/PrestaShop.git .
 
-git clone --depth 1 --branch ${PRESTASHOP_VERSION} https://github.com/PrestaShop/PrestaShop.git /var/www/html
+/usr/bin/composer install --no-interaction
+npm -g install npm
+npm install
+chmod +x tools/assets/build.sh
+./tools/assets/build.sh
 
-chown -R www-data /var/www/html
-runuser -g www-data -u www-data -- /usr/bin/composer install --no-interaction
-runuser -g www-data -u www-data -- /usr/bin/make assets
-
-mkdir log app/logs
-chmod +w -R admin-dev/autoupgrade app/config app/logs app/Resources/translations cache config download img log mails modules themes translations upload var
+mkdir -p log app/logs
+#chmod +w -R admin-dev/autoupgrade app/config app/logs app/Resources/translations cache config download img log mails modules themes translations upload var
+chown -R www-data:www-data /var/www/html
 
 php install-dev/index_cli.php --db_server=db --db_password=root --db_name=prestashop --name=ExampleShop --country=de --language=de --firstname=Max --lastname=Musterman --password=admin123 --email=admin@admin.com
 
 mv install-dev __install-dev
-mv admin* backend
 
 tail -f /dev/stdout
